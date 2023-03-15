59e60fb8d9da8909ce072eded24cbb2a03099a5e
Yu-Cheng Chang <45489725+Yucheng0208@users.noreply.github.com>
2023-03-15 08:51:55 +0800

    Automatically parse the path and read Json

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import json
+import time
 
+file_path = "./json/"
+file_i = 0
 pose_kepoints = []
-    
-with open('000000000031_keypoints.json') as f:
-    data = json.load(f)
-for people_item in data['people']:
-    pose_kepoints = people_item['pose_keypoints_2d']
-    print(pose_kepoints)
-    print(type(people_item))
+filename_keywords = "_keypoints.json"
+filename = "000000000000__keypoints.json"
+# 設定名稱迴圈以及字串合成
+for file_i in range(1000000000000):
+    filename = file_path + f'{file_i:012}' + filename_keywords
+    #print(filename)
+
+# 開啟json
+    with open(filename) as f:
+        data = json.load(f)
+
+    # 取得people底下的pose_keypoints_2d
+    for people_item in data['people']:
+        pose_kepoints = people_item['pose_keypoints_2d']
+        #print(pose_kepoints)
